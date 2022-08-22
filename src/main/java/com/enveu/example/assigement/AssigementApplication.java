2e7753870786cc71a3972482195b7f7aac4a7645
Manish Kumar <mk944939kumar@gmail.com>
2022-08-22 15:07:10 +0530

    added author in classes

--- src/main/java/com/enveu/example/assigement/AssigementApplication.java
+++ src/main/java/com/enveu/example/assigement/AssigementApplication.java
@@ -3,7 +3,7 @@ package com.enveu.example.assigement;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*@Author Manish Kumar*/
 @SpringBootApplication
 public class AssigementApplication implements CommandLineRunner {
 
